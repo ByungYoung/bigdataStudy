f64eabf6c4859d54d25f509dd54b5183453130e1
NachoCookie <sergiopark10373@likelion.org>
2022-08-23 16:41:16 +0900

    class1. Variable
    class1. Variable

---
+++ javaStudy_0823/src/javaStudy_0823/Ex01.java
@@ -0,0 +1,13 @@
+package javaStudy_0823;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// 실행 단축어 : ctrl + F11
+		// main method : 프로그램의 첫번째 상태
+		System.out.println("Java");		//System.out.println("내용") : 사용자가 입력한 내용을 출력
+		System.out.println("DB");
+		System.out.println(2+5+"10"+3);
+		System.out.println();			//syso + ctrl + space -> 자동완성
+	}
+}
---
+++ javaStudy_0823/src/javaStudy_0823/Ex02.java
@@ -0,0 +1,21 @@
+package javaStudy_0823;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		//변수명 규칙
+		//1. 특수기호 사용불가 : 예외(_)
+		//2. 키워드 사용 불가
+		//3. 띄어쓰기 불가
+		//4. 숫자로 시작 불가
+		//Camel 표기법(int stringBuffer), Snake 표기법(int string_buffer)
+		
+		//변수(Variable) : 변할 수 있는 값
+		int a = 5;			//정수형 변수 선언 및 초기화
+		System.out.println(a);
+		char b = 97;		//문자열 변수 선언 및 초기화
+		System.out.println(b);
+		float c = 10.5f;
+		double d = 10.5;
+	}
+}
