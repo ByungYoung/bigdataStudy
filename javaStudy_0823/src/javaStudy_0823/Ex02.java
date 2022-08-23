2b92310e2651203f62d68d2ca5707794499d76a2
NachoCookie <sergiopark10373@likelion.org>
2022-08-23 17:01:58 +0900

    Class1. Variable
    Class1. Variable

--- javaStudy_0823/src/javaStudy_0823/Ex02.java
+++ javaStudy_0823/src/javaStudy_0823/Ex02.java
@@ -8,6 +8,7 @@ public class Ex02 {
 		//2. 키워드 사용 불가
 		//3. 띄어쓰기 불가
 		//4. 숫자로 시작 불가
+		//5. 변수명 재선언 불가
 		//Camel 표기법(int stringBuffer), Snake 표기법(int string_buffer)
 		
 		//변수(Variable) : 변할 수 있는 값
@@ -15,7 +16,10 @@ public class Ex02 {
 		System.out.println(a);
 		char b = 97;		//문자열 변수 선언 및 초기화
 		System.out.println(b);
-		float c = 10.5f;
+		float c = 10.5f;	//소수값 저장 키워드 : float, double
 		double d = 10.5;
+		int num = 30;
+		//데이터 연산 시 : 더 큰 자료형으로 형 변환
+		System.out.println(c+num);
 	}
 }
