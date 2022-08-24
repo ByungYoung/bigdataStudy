e5693ec5a7e5d78582448b2b774c67eafe36175f
NachoCookie <sergiopark10373@likelion.org>
2022-08-24 17:48:58 +0900

    Class2.Operator
    08.24

---
+++ javaStudy_0824/src/javaStudy_0824/Ex06.java
@@ -0,0 +1,20 @@
+package javaStudy_0824;
+
+import java.util.Scanner;
+
+public class Ex06 {
+	public static void main(String[] args) {
+//		int num = 7;
+//		if(num == 3) System.out.println("숫자 3입니다.");
+//		System.out.println("프로그램 종료");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("나이를 입력하세요 >> ");
+		int age = sc.nextInt();
+		
+		sc.close();
+		
+		if(age >= 20) System.out.println("성인입니다.");
+		else System.out.println("성인이 아닙니다.");
+	}
+}
