f28727247c550cda07b47fa68aed4be5a55d7188
NachoCookie <sergiopark10373@likelion.org>
2022-08-25 17:53:01 +0900

    Class03.Function
    08.25

--- javaStudy_0825/src/javaStudy_0825/Ex03.java
+++ javaStudy_0825/src/javaStudy_0825/Ex03.java
@@ -20,26 +20,18 @@ public class Ex03 {
 //		else System.out.println("점수가 0보다 작거나 100보다 클 수 없습니다.");
 		if(grade >= 0 && grade <= 100) {
 			switch(grade / 10) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-				System.out.println("휴학을 추천합니다.");
-				break;
-			case 7:
-				System.out.println("C 학점입니다.");
+			case 10:
+			case 9:
+				System.out.println("A 학점입니다.");
 				break;
 			case 8:
 				System.out.println("B 학점입니다.");
 				break;
-			case 9:
-				System.out.println("A 학점입니다.");
+			case 7:
+				System.out.println("C 학점입니다.");
 				break;
-			case 10:
-				System.out.println("A 학점입니다.");
+			default:
+				System.out.println("휴학을 추천합니다.");
 				break;
 			}
 		}
