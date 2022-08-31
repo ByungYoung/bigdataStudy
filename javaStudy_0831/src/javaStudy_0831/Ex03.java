a9d1bc0664ef8b4b846d871cf032f7bf09c4c9c4
NachoCookie <sergiopark10373@likelion.org>
2022-08-31 17:16:39 +0900

    Class05.Array
    22.08.31

--- javaStudy_0831/src/javaStudy_0831/Ex03.java
+++ javaStudy_0831/src/javaStudy_0831/Ex03.java
@@ -1,9 +1,20 @@
 package javaStudy_0831;
 
+import java.util.Scanner;
+
 public class Ex03 {
 	public static void main(String[] args) {
-		int[] arr;
-		arr = (1,2,3,4,5);
+		int[] numArray = new int[4];
+		Scanner sc = new Scanner(System.in);
 		
+		for(int i = 0; i < 4; i++) {
+			System.out.print("숫자 입력 >> ");
+			numArray[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			System.out.println(i+1 + " 번째 숫자 " + numArray[i]);
+		}
+		sc.close();
 	}
 }
