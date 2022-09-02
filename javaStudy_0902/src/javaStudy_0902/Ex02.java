46aa9dfcc755afab31f1c0513874f5d12c78b0a5
NachoCookie <sergiopark10373@likelion.org>
2022-09-02 17:16:00 +0900

    Class05.Array
    22.09.02 Multiple Array

--- javaStudy_0902/src/javaStudy_0902/Ex02.java
+++ javaStudy_0902/src/javaStudy_0902/Ex02.java
@@ -4,15 +4,18 @@ public class Ex02 {
 	public static void main(String[] args) {
 		int count = 0;
 		int[][] array = new int [5][5];
+		int verLen = array[0].length;
+		int parLen = array.length;
+		
 		//진행순서 (좌 -> 우)
-		for(int i = 0; i < array.length; i++) {
-			for(int j = 0; j < array[i].length; j++) {
+		for(int i = 0; i < parLen; i++) {
+			for(int j = 0; j < verLen; j++) {
 				array[i][j] = ++count;
 			}
 		}
 		
-		for(int i = 0; i < array.length; i++) {
-			for(int j = 0; j < array[i].length; j++) {
+		for(int i = 0; i < parLen; i++) {
+			for(int j = 0; j < verLen; j++) {
 				System.out.print(array[i][j] + "\t");
 			}
 			System.out.println();
@@ -20,14 +23,14 @@ public class Ex02 {
 		
 		System.out.println();
 		//진행순서 (위 -> 아래)
-		for(int i = 0; i < array.length; i++) {
-			for(int j = 0; j < array[i].length; j++) {
+		for(int i = 0; i < parLen; i++) {
+			for(int j = 0; j < verLen; j++) {
 				array[j][i] = ++count;
 			}
 		}
 		
-		for(int i = 0; i < array.length; i++) {
-			for(int j = 0; j < array[i].length; j++) {
+		for(int i = 0; i < parLen; i++) {
+			for(int j = 0; j < verLen; j++) {
 				System.out.print(array[i][j] + "\t");
 			}
 			System.out.println();
--- javaStudy_0902/src/javaStudy_0902/Ex03.java
+++ javaStudy_0902/src/javaStudy_0902/Ex03.java
@@ -4,16 +4,49 @@ public class Ex03 {
 	public static void main(String[] args) {
 		int[][] array = new int[5][5];
 		int count = 20;
-		
+//		//순서 (오른쪽 아래 -> 왼쪽 위)
+//		for(int i = 0; i < array.length; i++) {
+//			for(int j = 0; j < array[i].length; j++) {
+//				array[j][i] = ++count;
+//			}
+//		}
+//		
+//		for(int i = 0; i < array.length; i++) {
+//			for(int j = 0; j < array[i].length; j++) {
+//				System.out.print(array[i][array[i].length-1-j] + "\t");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		count = 20;
+//		//순서 (우 -> 좌)
+//		for(int i = 0; i < array.length; i++) {
+//			for(int j = 0; j < array[i].length; j++) {
+//				array[i][j] = ++count;
+//			}
+//		}
+//		
+//		for(int i = 0; i < array.length; i++) {
+//			for(int j = 0; j < array[i].length; j++) {
+//				System.out.print(array[i][array[i].length-1-j] + "\t");
+//			}
+//			System.out.println();
+//		}
 		for(int i = 0; i < array.length; i++) {
 			for(int j = 0; j < array[i].length; j++) {
-				array[j][i] = ++count;
+				array[i][j] = ++count;
 			}
 		}
-		
 		for(int i = 0; i < array.length; i++) {
-			for(int j = 0; j < array[i].length; j++) {
-				System.out.print(array[i][j] + "\t");
+			if(i%2 == 0) {
+				for(int j = 0; j < array[i].length; j++) {
+					System.out.print(array[i][j] + "\t");
+				}
+			}
+			else {
+				for(int j = 0; j < array[i].length; j++) {
+					System.out.print(array[i][array[i].length - 1 - j] + "\t");
+				}
 			}
 			System.out.println();
 		}
